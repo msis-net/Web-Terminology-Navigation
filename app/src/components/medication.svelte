e1123647850a2493da37c787bab6183a1ddacc8e
Akiyama <mail@msis-net.com>
2024-07-05 08:31:31 +0900

    main

---
+++ app/src/components/medication.svelte
@@ -0,0 +1,33 @@
+<div class="m-2">
+  <small>
+    iframeを使っている為、通常では"X-Frame-Options"が発生し表示されません。
+    <br />Chromeを利用し、<a
+      href="https://chromewebstore.google.com/detail/ignore-x-frame-headers/gleekbfjekiniecknbkamfmkohkpodhe"
+      target="_blank"
+      rel="noopener noreferrer">「Ignore X-Frame header」</a
+    >プラグインを利用してください。(通常のブラウジングでIgnore X-Frame
+    headerはオフにしてください)</small
+  >
+</div>
+
+<div class="flex space-x-1 p-2 content-wrapp">
+  <section class=" border w-full">
+    <div>
+      右で選択した内容がここに表示されるイメージですが、ここでの機能は実装がありません。
+    </div>
+    <!--
+    <iframe
+      src="https://fms.annyys.net/ad_web/tensu-order-demo.html"
+      frameborder="0"
+      class="h-full w-full"
+    ></iframe>
+    -->
+  </section>
+  <section class=" border w-full">
+    <iframe
+      src="https://fms.annyys.net/ad_web/tensu_demo.html"
+      frameborder="0"
+      class="h-full w-full"
+    ></iframe>
+  </section>
+</div>
